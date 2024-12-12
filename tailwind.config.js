8d2d09630f014a1904a484faea7c99cb503a62bf
kamalnath69 <kamalnath9669@gmail.com>
2024-12-12 10:22:34 +0530

    Testing 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	prefix: 'tw-',
-	important: false,
+	prefix: 'tw-', // Adds "tw-" prefix to all Tailwind classes
+	important: false, // If true, will apply Tailwind styles with !important
 	content: [
-		"**/*.{html, jsx, js}",
-		"**/*.js",
-		"**/*.html",
+	  "**/*.{html,jsx,js}", // Scan all HTML, JSX, and JS files in your project
+	  "**/*.js", // Also scan all JS files
+	  "**/*.html", // Also scan all HTML files
 	],
-	darkMode: 'class',
+	darkMode: 'class', // Enables manual dark mode using class (use "dark" class to enable dark mode)
 	theme: {
-		extend: {
-			 
-			fontFamily: {
-				poly: ['"poly"', "serif"],
-			},
+	  extend: {
+		colors: {
+		  'custom-light': '#e2dada', // Example of adding a custom color (light mode)
+		  'custom-dark': '#000000',  // Example of adding a custom color (dark mode)
 		},
+		fontFamily: {
+		  poly: ['"poly"', "serif"], // Example of adding a custom font family
+		},
+	  },
 	},
 	plugins: [
-		function({ addVariant }) {
-			addVariant('firefox', ':-moz-any(&)')
-		}
+	  function({ addVariant }) {
+		addVariant('firefox', ':-moz-any(&)'); // Custom variant for Firefox
+	  }
 	],
-}
-
+  }
+  
